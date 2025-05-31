f6e5f420c92a07f35fe04e58d364960ff46dbebb
mangabaycjake <mangabaychristianjake@gmail.com>
2025-05-31 14:26:31 +0800

    Try synch

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
   return (
     <div className={`container ${theme}`}>
       <Navbar theme={theme} setTheme={setTheme} />
+      <h1>Header</h1>
+      <div className="content">
+        <p>This is some text on the left side.</p>
+        <img src="https://via.placeholder.com/150" alt="Example" />
+      </div>
     </div>
   )
 }
